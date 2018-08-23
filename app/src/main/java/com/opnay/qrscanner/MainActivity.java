9b0d371315509e6689709f22fb08ea17f46f6d9c
Kim In Seop <opnayn@gmail.com>
2018-08-23 16:34:34 +0900

    QRCode Reader. from https://www.youtube.com/watch?v=bZRmrxSXJSc

--- app/src/main/java/com/opnay/qrscanner/MainActivity.java
+++ app/src/main/java/com/opnay/qrscanner/MainActivity.java
@@ -1,13 +1,92 @@
 package com.opnay.qrscanner;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements Detector.Processor<Barcode> {
+
+    private TextView tv;
+    private SurfaceView sfv;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+        sfv = (SurfaceView) findViewById(R.id.surfaceView);
+
+        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.ALL_FORMATS).build();
+        barcodeDetector.setProcessor(this);
+
+        cameraSource = new CameraSource.Builder(getApplicationContext(), barcodeDetector)
+                .setRequestedPreviewSize(1024, 768).setAutoFocusEnabled(true)
+                .build();
+
+        final Activity activity = this;
+        sfv.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(activity, new String[] {Manifest.permission.CAMERA}, 1024);
+                        return;
+                    }
+                    cameraSource.start(sfv.getHolder());
+                } catch (IOException e) {
+                    Log.e("MainActivity", e.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections<Barcode> detections) {
+        final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+        if (barcodes.size() != 0) {
+            final StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < barcodes.size(); i++)
+                sb.append(barcodes.valueAt(i).rawValue).append("\n");
+            tv.post(new Runnable() {
+                @Override
+                public void run() {
+                    tv.setText(sb.toString());
+                }
+            });
+        }
     }
 }
